0280ad1537630765c140b62c61ca4a28112f38e3
heraxusitory <vladiksmart@gmail.com>
2020-11-20 17:54:49 +0300

    Done migrations

---
+++ database/migrations/2020_11_20_121059_create_book_author_genres_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookAuthorGenresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('book_author_genres', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('genre_id');
+            $table->foreign('book_id')->references('id')->on('books');
+            $table->foreign('genre_id')->references('id')->on('genres');
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_author_genres');
+    }
+}
