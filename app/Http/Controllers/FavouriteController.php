2948b61a9a4e964fc9595578ea1ba839d334d924
heraxusitory <vladiksmart@gmail.com>
2020-11-22 07:50:55 +0300

    fix

--- app/Http/Controllers/FavouriteController.php
+++ app/Http/Controllers/FavouriteController.php
@@ -9,4 +9,12 @@ class FavouriteController extends Controller
     public function showFavourites() {
         return view('favourites.index');
     }
+
+    public function add() {
+//        return view('books');
+    }
+
+    public function delete() {
+
+    }
 }
