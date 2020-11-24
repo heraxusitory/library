5428e1810991e7c019901d67016f2800286eee30
heraxusitory <vladiksmart@gmail.com>
2020-11-24 14:11:03 +0300

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', 'BookController@showBooks')->name('books');
-Route::get('/books/{book_id}', 'BookController@showBook')->name('book.show');
-Route::delete('/drop/{book_id}', 'BookController@dropBook')->name('book.drop');
 
+Route::prefix('genres')->middleware('admin')->group(function () {
+    Route::get('/create/genre', 'ModalController@formGenreCreate')->name('genre.get.create');
+});
 
-Route::prefix('profile')->middleware('auth')->group(function() {
+Route::prefix('books')->group(function () {
+
+    Route::get('/{book_id}', 'BookController@showBook')->name('book.show');
+
+    Route::middleware('admin')->group(function () {
+        Route::get('/edit/{book_id}', 'ModalController@formBookEdit')->name('book.get.edit');
+        Route::put('/edit/{book_id}', 'BookController@update')->name('book.update');
+        Route::get('/create/book', 'ModalController@formBookCreate')->name('book.get.create');
+        Route::put('/create', 'BookController@create')->name('book.create');
+        Route::delete('/drop/{book_id}', 'BookController@dropBook')->name('book.drop');
+    });
+
+});
+
+Route::prefix('authors')->middleware('admin')->group(function () {
+    Route::get('/create/author', 'ModalController@formAuthorCreate')->name('author.get.create');
+    Route::put('/create', 'AuthorController@create')->name('author.create');
+});
+
+Route::prefix('profile')->middleware('auth')->group(function () {
     Route::get('/', 'ProfileController@showProfile')->name('profile');
     Route::get('/favourites/{user_id}', 'FavouriteController@showFavourites')->name('favourites');
 });
 
-Route::prefix('favourites')->middleware('auth')->group(function() {
+Route::prefix('favourites')->middleware('auth')->group(function () {
     Route::post('/add/{book_id}', 'FavouriteController@add')->name('favourites.add');
     Route::delete('/delete/{book_id}', 'FavouriteController@delete')->name('favourites.delete');
 });
@@ -32,7 +52,7 @@ Route::prefix('authors')->group(function () {
     Route::get('/{author_id}', 'AuthorController@showAuthor')->name('author.show');
 });
 
-Route::prefix('genres')->group(function() {
+Route::prefix('genres')->group(function () {
     Route::get('/', 'GenreController@showGenres')->name('genres');
     Route::get('/{genre_id}', 'GenreController@showGenre')->name('genre.show');
 });
