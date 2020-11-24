0be2d89249a45c0084e229a0896e1dbfa9d28584
heraxusitory <vladiksmart@gmail.com>
2020-11-24 14:06:40 +0300

    Added controller for modal window

---
+++ app/Http/Controllers/ModalController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use App\Book;
+use App\BookAuthorGenre;
+use App\Genre;
+use Illuminate\Http\Request;
+
+class ModalController extends Controller
+{
+
+    public function formBookEdit( Author $authorM, Genre $genreM, BookAuthorGenre $bookFullM, $bookId) {
+//        $book = $bookM->findBookById($bookId);
+        $authors = $authorM->getAuthors();
+        $genres = $genreM->getGenres();
+        $book = $bookFullM->getFullBookById($bookId);
+//        return response()->json(['html' => $book]);
+        $form = view('modals.books.edit', compact('book', 'authors', 'genres'))->render();
+        return response()->json(['html' => $form]);
+    }
+
+    public function formBookCreate(Author $authorM, Genre $genreM) {
+        $authors = $authorM->getAuthors();
+        $genres = $genreM->getGenres();
+        $form = view('modals.books.create', compact('authors', 'genres'))->render();
+        return response()->json(['html' => $form]);
+
+
+    }
+
+    public function formAuthorCreate() {
+        $form = view('modals.authors.create')->render();
+        return response()->json(['html' => $form]);
+    }
+
+    public function formGenreCreate() {
+        $form = view('modals.genres.create')->render();
+        return response()->json(['html' => $form]);
+    }
+}
